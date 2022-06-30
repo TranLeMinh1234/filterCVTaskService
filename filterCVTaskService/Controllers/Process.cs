af45f54df7dbbe0e0d1891bd1b727db77e74f3a0
TranLeMinh1234 <tlMinh10300@gmail.com>
2022-06-30 22:34:32 +0700

    tlminh: sua bug

--- filterCVTaskService/Controllers/Process.cs
+++ filterCVTaskService/Controllers/Process.cs
@@ -156,7 +156,14 @@ namespace filterCVTaskService.Controllers
 
         private void SortCVByNumberOfMatchedCriteria()
         {
-            suitableList.Sort((x, y) => x.numberOfMatched > y.numberOfMatched);
+            suitableList.Sort((x, y) => {
+                if (x.numberOfMatched > y.numberOfMatched)
+                    return -1;
+                else if (x.numberOfMatched < y.numberOfMatched)
+                    return 1;
+                else 
+                    return 0;
+            });
         }
 
         private async Task GetTextInfoCV(HttpClient httpClient)
